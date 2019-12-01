a5b89bf9c81bfa0cdb9993486f7548d8a2150108
akitaSummerqq.com <644171127@qq.com>
2019-12-01 22:47:33 +0800

    20191201 22:47 update

--- interview/coding/throttle.js
+++ interview/coding/throttle.js
@@ -10,3 +10,20 @@ function throttle(func, delay = 500) {
         }
     }
 }
+
+function repeat(func, times, wait) {
+    let i = 0
+    return function(str) {
+        const I = setInterval(() => {
+            console.log(wait)
+            if (i >= wait) {
+                clearInterval(I)
+            } else {
+                i++
+                func(str)
+            }
+        }, times)
+    }
+}
+const repeatFunc = repeat(alert, 4, 3000)
+repeatFunc("hellworld")
