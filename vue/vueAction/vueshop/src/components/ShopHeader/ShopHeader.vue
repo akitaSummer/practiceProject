b934995475b3d311471b02c340761779f90ef39b
akitaSummerqq.com <644171127@qq.com>
2020-02-07 23:51:34 +0800

    20200207 23:51 update

--- vue/vueAction/vueshop/src/components/ShopHeader/ShopHeader.vue
+++ vue/vueAction/vueshop/src/components/ShopHeader/ShopHeader.vue
@@ -41,7 +41,91 @@
     </div>
 
     <div class="shop-brief-modal">
-      <div class="biref-modal-content"></div>
+        <div class="brief-modal-content">
+          <h2 class="content-title">
+            <span class="content-tag">
+            <span class="mini-tag">品牌</span>
+            </span>
+            <span class="content-name">嘉禾一品（温都水城）</span>
+          </h2>
+          <ul class="brief-modal-msg">
+            <li>
+              <h3>3.5</h3>
+              <p>评分</p>
+            </li>
+            <li>
+              <h3>90 单</h3>
+              <p>月售</p>
+            </li>
+            <li>
+              <h3>硅谷专送</h3>
+              <p>约 28 分钟</p>
+            </li>
+            <li>
+              <h3>4 元</h3>
+              <p>配送费用</p>
+            </li>
+            <li>
+              <h3>1000m</h3>
+              <p>距离</p>
+            </li>
+          </ul>
+          <h3 class="brief-modal-title"><span>公告</span></h3>
+          <div class="brief-modal-notice">
+            是以粥为特色的中式营养快餐，自 2004 年 10 月 18 日创立“嘉和一品”品牌至今
+          </div>
+          <div class="mask-footer">
+            <span class="iconfont icon-close"></span>
+          </div>
+        </div>
+        <div class="brief-modal-cover"></div>
+    </div>
+    <div class="activity-sheet">
+      <div class="activity-sheet-content">
+        <h2 class="activity-sheet-title">优惠活动</h2>
+        <ul class="list">
+          <li class="activity-item activity-green">
+            <span class="content-tag">
+              <span class="mini-tag">首单</span>
+            </span>
+            <span class="activity-content">新用户下单立减 17 元(不与其它活动同享)</span>
+          </li>
+          <li class="activity-item activity-red">
+            <span class="content-tag">
+              <span class="mini-tag">满减</span>
+            </span>
+            <span class="activity-content">满 35 减 19，满 65 减 35</span>
+          </li>
+          <li class="activity-item activity-orange">
+            <span class="content-tag">
+              <span class="mini-tag">特价</span>
+            </span>
+            <span class="activity-content">【立减 19.5 元】欢乐小食餐</span>
+          </li>
+          <li class="activity-item activity-green">
+            <span class="content-tag">
+              <span class="mini-tag">首单</span>
+            </span>
+            <span class="activity-content">新用户下单立减 17 元(不与其它活动同享)</span>
+          </li>
+          <li class="activity-item activity-red">
+            <span class="content-tag">
+              <span class="mini-tag">满减</span>
+            </span>
+            <span class="activity-content">满 35 减 19，满 65 减 35</span>
+          </li>
+          <li class="activity-item activity-orange">
+            <span class="content-tag">
+              <span class="mini-tag">特价</span>
+            </span>
+            <span class="activity-content">【立减 19.5 元】欢乐小食餐</span>
+          </li>
+        </ul>
+        <div class="activity-sheet-close">
+          <span class="iconfont icon-close"></span>
+        </div>
+      </div>
+      <div class="activity-sheet-cover"></div>
     </div>
   </div>
 </template>
@@ -248,6 +332,223 @@
           transform: translateY(-50%);
           right: 0;
         }
+      }
+    }
+    .shop-brief-modal {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 52;
+      flex-direction: column;
+      color: #333;
+      .brief-modal-cover {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, .5);
+        z-index: 1;
+      }
+      .brief-modal-content {
+        position: relative;
+        width: 80%;
+        padding: 25px 20px;
+        border-radius: 5px;
+        background: #fff;
+        z-index: 99;
+        display: flex;
+        flex-direction: column;
+        .content-title {
+          font-size: 20px;
+          line-height: 24px;
+          font-weight: 700;
+          white-space: nowrap;
+          position: relative;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          > span {
+            font-weight: 600;
+          }
+          .content-tag {
+            border-radius: 2px;
+            background-image: linear-gradient(90deg, #fff100, #ffe339);
+            width: 36px;
+            height: 18px;
+            margin-right: 10px;
+            color: #6a3709;
+            font-style: normal;
+            position: relative;
+            .mini-tag {
+              position: absolute;
+              left: 0;
+              top: 0;
+              right: -100%;
+              bottom: -100%;
+              font-size: 24px;
+              transform: scale(.5);
+              transform-origin: 0 0;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+          }
+        }
+      }
+      .brief-modal-msg {
+        display: flex;
+        margin: 20px -10px 0;
+        > li {
+          flex: 1;
+          text-align: center;
+          > h3 {
+            font-size: 15px;
+            font-weight: 600;
+            color: #333;
+            margin-bottom: 8px;
+          }
+          > p {
+            font-size: 12px;
+            color: #999;
+          }
+        }
+      }
+      .brief-modal-title {
+        position: relative;
+        text-align: center;
+        margin: 15px auto 15px;
+        width: 85px;
+        background-image: linear-gradient(90deg, #fff, #333 50%, #fff);
+        background-size: 100% 1px;
+        background-position: 50%;
+        background-repeat: no-repeat;
+        > span {
+          font-size: 12px;
+          padding: 0 6px;
+          color: #999;
+          background: #fff;
+        }
+      }
+      .brief-modal-notice {
+        font-size: 13px;
+        line-height: 1.54;
+        color: #333333;
+        overflow-y: auto;
+      }
+      .mask-footer {
+        position: absolute;
+        bottom: -60px;
+        left: 50%;
+        padding: 6px;
+        border: 1px solid rgba(255, 255, 255, .7);
+        border-radius: 50%;
+        transform: translateX(-50%);
+        span {
+          font-size: 16px;
+          color: rgba(255, 255, 255, .7)
+        }
+      }
+    }
+
+    .activity-sheet {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 99;
+      .activity-sheet-content {
+        position: absolute;
+        background-color: #f5f5f5;
+        box-shadow: 0 -1px 5px 0 rgba(0, 0, 0, .4);
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 100;
+        padding: 20px 30px;
+        box-sizing: border-box;
+        transition: transform .2s;
+        will-change: transform;
+        color: #333;
+        .activity-sheet-title {
+          text-align: center;
+          font-size: 20px;
+          font-weight: 600;
+          margin-bottom: 20px
+        }
+        .list {
+          font-size: 16px;
+          height: 160px;
+          overflow-y: auto;
+          .activity-item {
+            margin-bottom: 12px;
+            display: flex;
+            font-size: 13px;
+            align-items: center;
+            &.activity-green {
+              .content-tag {
+                background-color: rgb(112, 188, 70);
+              }
+            }
+            &.activity-red {
+              .content-tag {
+                background-color: rgb(240, 115, 115);
+              }
+            }
+            &.activity-orange {
+              .content-tag {
+                background-color: rgb(241, 136, 79);
+              }
+            }
+            .content-tag {
+              display: inline-block;
+              border-radius: 2px;
+              width: 36px;
+              height: 18px;
+              margin-right: 10px;
+              color: #fff;
+              font-style: normal;
+              position: relative;
+              .mini-tag {
+                position: absolute;
+                left: 0;
+                top: 0;
+                right: -100%;
+                bottom: -100%;
+                font-size: 24px;
+                transform: scale(.5);
+                transform-origin: 0 0;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+              }
+            }
+          }
+        }
+        .activity-sheet-close {
+          position: absolute;
+          right: 6px;
+          top: 10px;
+          width: 25px;
+          height: 25px;
+          > span {
+            font-size: 20px
+          }
+        }
+      }
+      .activity-sheet-cover {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, .5);
       }
     }
   }
