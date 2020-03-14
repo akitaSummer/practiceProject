a48dc37b6fb946c582fe1cef2f58ec303bdac34f
akitaSummerqq.com <644171127@qq.com>
2020-03-14 23:47:49 +0800

    20200314 23:47 update

--- interview/coding/i.js
+++ interview/coding/i.js
@@ -1,10 +1,48 @@
-const single = (function() {
-    let simple = null
-    return function() {
-        if (simple === null) {
-            return {}
-        } else {
-            return simple
+function test(arr) {
+    const result = []
+    const testArr = arr[1].split(' ').map(item => {
+        return Number(item)
+    })
+    const zeroQ = testArr.reduce((pre, item) => {
+        return pre * item
+    }, 1)
+    result.push(mod(zeroQ))
+    let oneQ = []
+    for (let i = 0; i < testArr.length; i++) {
+        const te = [...testArr]
+        te[i] = te[i] / (te[i] - 1)
+        let teR = testArr.reduce((pre, item) => {
+            return pre * item
+        }, 1)
+        oneQ.push(1 / (teR))
+    }
+    oneQ = 1 / (oneQ.reduce((pre, item) => {
+        return pre + item
+    }, 0))
+    result.push(mod(oneQ))
+    let twoQ = []
+    for (let i = 0; i < testArr.length; i++) {
+        for (let j = i + 1; j < testArr.length; j++) {
+            const te = [...testArr]
+            te[i] = te[i] / (te[i] - 1)
+            te[j] = te[j] / (te[j] - 1)
+            let teR = testArr.reduce((pre, item) => {
+                return pre * item
+            }, 1)
+            twoQ.push(1 / (teR))
         }
     }
-})()
+    twoQ = 1 / (twoQ.reduce((pre, item) => {
+        return pre + item
+    }, 0))
+    result.push(mod(twoQ))
+    console.log(result)
+}
+
+function mod(num) {
+    if (num === 1) {
+        return 1
+    }
+    return Math.floor((998244353 - 998244353 / num) * mod(998244353 % num) % 998244353) + 1
+}
+test(['2', '2 2'])
---
+++ sudoku/src/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //自动生成html文件的插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //清除之前打包的文件
+
+module.exports = {
+    entry: '',
+    output: {
+        path: path.resolve(__dirname, '../src/js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }, {
+                loader: 'sass-loader'
+            }]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, '../src/index.html') }),
+        new CleanWebpackPlugin()
+    ]
+}
