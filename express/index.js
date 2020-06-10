17151161d023b9a9b9d5d1e749983951cb09b884
liuyu14@corp.netease.com <liuyu14@corp.netease.com>
2020-06-10 23:26:38 +0800

    20200610 23:26 update

--- express/index.js
+++ express/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json()) // 允许express解析post中的json到req.body中
+
 const mongoose = require('mongoose') // 引入mongoose
 mongoose.connect('mongodb://localhost:27017/express-test', { useNewUrlParser: true, useUnifiedTopology: true }) // 连接mongodb数据库
 const Product = mongoose.model('Product', new mongoose.Schema({
@@ -18,7 +20,36 @@ app.get('/', function(req, res) {
 })
 
 app.get('/products', async function(req, res) {
-    res.send(await Product.find())
+    // const data = await Product.find().skip(1).limit(2)
+    // const data = await Product.where({ title: 'Product B' })
+    const data = await Product.sort({ _id: -1 }) // 1是正序，-1是倒叙
+    res.send(data)
+})
+
+app.get('/products/:id', async(req, res) => {
+    const data = await Product.findById(req.params.id)
+    res.send(data)
+})
+
+app.post('/products', async(req, res) => {
+    const data = req.body
+    const product = await Product.create(data)
+    res.send(product)
+})
+
+app.put('/products/:id', async(req, res) => {
+    const product = await Product.findById(req.params.id)
+    product.title = req.body.title
+    await product.save()
+    res.send(product)
+})
+
+app.delete('/products/:id', async(req, res) => {
+    const product = await Product.findById(req.params.id)
+    await product.remove()
+    res.send({
+        success: true
+    })
 })
 
 app.listen(4000, () => {
