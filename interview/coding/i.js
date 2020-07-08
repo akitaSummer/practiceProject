892d9216fce0dae46320f268e057e8f9e4a01ca2
akitasummer <644171127@qq.com>
2020-07-08 23:14:19 +0800

    20200708 23:14 update

--- interview/coding/i.js
+++ interview/coding/i.js
@@ -1,242 +1,171 @@
-// // function DistanceToHigher(height) {
-// //     let count = 0
-// //     const result = [0]
-// //     for (let i = 1; i < height.length; i++) {
-// //         for (let j = i - 1; j >= 0; j--) {
-// //             if (height[j] > height[i]) {
-// //                 result.push(i - j)
-// //                 break
-// //             }
-// //             if (j === 0) {
-// //                 result.push(0)
-// //             }
-// //         }
-// //     }
-// //     return result
-// // }
-
-// // console.log(DistanceToHigher([175, 179, 174, 163, 176, 177]))
-
-// function gcd(a, b) {
-//     let temp
-//     while (b !== 0) {
-//         temp = a % b
-//         a = b
-//         b = temp
-//     }
-//     return a
-// }
-
-// // function test(str) {
-// //     const content = str.split('\n')
-// //     const resultNums = content[0].split(' ').map(item => Number(item))
-// //     const result = new Array(resultNums[1])
-// //     for (let i = 0; i < result.length; i++) {
-// //         result[i] = new Array(resultNums[0] + 1)
-// //     }
-// //     const people = new Array(resultNums[0])
-// //     for (let i = 1, j = 0; i <= resultNums[0]; i++, j++) {
-// //         people[j] = content[i].split('')
-// //     }
-// //     const need = []
-// //     for (let i = resultNums[0] + 1; i < content.length - 1; i++) {
-// //         need.push(content[i].split(' '))
-// //     }
-// //     const needY = []
-// //     for (let i = 0; i < need.length; i++) {
-// //         if (need[i][2] === 'Y') {
-// //             needY.push(need[i])
-// //         }
-// //     }
-// //     const needN = []
-// //     for (let i = 0; i < need.length; i++) {
-// //         if (need[i][2] === 'N') {
-// //             needN.push(need[i])
-// //         }
-// //     }
-// //     const com = []
-// //     for (let i = 0; i < needY.length; i++) {
-// //         const res = [null, null, null]
-// //         res[Number(needY[i][0]) - 1] = people[Number(needY[i][0]) - 1][Number(needY[i][1]) - 1]
-// //         res[Number(needY[i][3]) - 1] = people[Number(needY[i][3]) - 1][Number(needY[i][4]) - 1]
-// //         res.push(needY[i][0] + needY[i][3])
-// //         com.push(res)
-// //     }
-// //     for (let i = 0; i < com.length; i++) {
-// //         for (let j = i + 1; j < com.length; j++) {
-// //             if (com[i]) {}
-// //             if (com[j][0] === com[i][0] || com[j][1] === com[i][0] || com[j][0] === com[i][1] || com[j][1] === com[i][1]) {
-// //                 com[i] = com[i].concat(com[j])
-// //                 com.splice(j, 1)
-// //                 i--
-// //                 break
-// //             }
-// //         }
-// //     }
-// //     for (let i = 0; i < com.length; i++) {
-// //         if (com[i].length > 3) {
-// //             com[i].splice(2, 1)
-// //             com[i].splice(4, 1)
-// //             com[i] = [...new Set(com[i])]
-// //         }
-// //     }
-// //     console.log(result, people, need, needY, needN, com)
-// // }
-
-// // function maxBoxes(boxes) {
-// //     // write code here
-// //     boxes = boxes.sort((a, b) => {
-// //         a = a.reduce((pre, item) => {
-// //             return pre * item
-// //         }, 1)
-// //         b = b.reduce((pre, item) => {
-// //             return pre * item
-// //         }, 1)
-// //         return a - b
-// //     })
-// //     let count = []
-// //     count[0] = 1
-// //     for (let i = 1; i < boxes.length; i++) {
-// //         if (boxes[i - 1][0] < boxes[i][0] && boxes[i - 1][1] < boxes[i][1] && boxes[i - 1][2] < boxes[i][2]) {
-// //             count[i] = count[i - 1] + 1
-// //         } else {
-// //             count[i] = count[i - 1]
-// //             boxes[i] = boxes[i - 1]
-// //         }
-// //     }
-// //     return count.pop()
-// // }
-
-// // console.log(maxBoxes([
-// //         [5, 6, 3],
-// //         [5, 4, 6],
-// //         [6, 6, 6]
-// //     ]))
-// // console.log(test('3 4\nABCD\nefgh\nIJKL\n1 1 Y 3 2\n1 2 N 2 2\n2 2 Y 3 4\n1 3 Y 2 3\n1 1 N 2 4\n3 1 Y 1 3\n0 0 N 0 0\n'))
-
-// // let input = ''
-
-// // while (line = readline()) {
-// //     input += line + '\n'
-// // }
-// // print(test(input))
-
-// // while (line = readline()) {
-// //     var lines = line.split(" ");
-// //     var a = parseInt(lines[0]);
-// //     var b = parseInt(lines[1]);
-// //     print(a + b);
-// // }
-
-// // var n = parseInt(readline());
-// // var ans = 0;
-// // for(var i = 0;i < n; i++){
-// //     lines = readline().split(" ")
-// //     for(var j = 0;j < lines.length; j++){
-// //         ans += parseInt(lines[j]);
-// //     }
-// // }
-// // print(ans);
-
-// // function test(num1, num2) {
-// //     const result = Number(num1) + Number(num2) + ''
-// //     const resultArr = result.split('')
-// //     let flag = 0
-// // for (let i = resultArr.length - 1; i >= 0; i--) {
-// //     resultArr[i] = resultArr[i] + flag
-// //     flag = 0
-// //     if (i >= 9) {
-// //         resultArr[i] = resultArr[i] - 9
-// //         flag = 1
-// //     }
-// //     if (i === 0 && flag === 1) {
-// //         resultArr.push(1)
-// //     }
-// // }
-// //     return resultArr.join('')
-// // }
-
-// function sumString(a, b) {
-//     let arrA = a + ''
-//     let arrB = b + ''
-//     arrA = arrA.split('')
-//     arrB = arrB.split('')
-//     const len = arrA.length - arrB.length
-//     let carry = 0
-//     if (len > 0) {
-//         for (let i = 0; i < len; i++) {
-//             arrB.unshift('0')
-//         }
-//     } else {
-//         for (let i = 0; i < Math.abs(len); i++) {
-//             arrA.unshift('0')
-//         }
-//     }
-//     const res = []
-//     for (let i = arrA.length - 1; i >= 0; i--) {
-//         let temp = +arrA[i] + (+arrB[i]) + carry
-//         if (temp >= 9) {
-//             carry = 1
-//             res.unshift((temp % 9 + ''))
-//         } else {
-//             carry = 0
-//             res.unshift((temp + ''))
-//         }
-//     }
-//     if (carry) {
-//         res.unshift(1 + '')
-//     }
-
-//     if (res.length === 1) {
-//         return res.join('')
-//     }
-//     return res.join('').replace(/^0/, '')
-// }
-
-// function add(num1, num2) {
-//     num1s = num1.split('.')
-//     num2s = num2.split('.')
-//     if (num1s[1] && num2s[1]) {
-//         let resultSm = sumString(num1s[1], num2s[1]).split('')
-//         let flag1 = '0'
-//         if (resultSm.length > num1s[1].split('').length && resultSm.length > num2s[1].split('').length) {
-//             flag1 = '1'
-//             resultSm.shift()
-//             resultSm = resultSm.join('')
-//         }
-//         let result = sumString(flag1, sumString(num1s[0], num2s[0]))
-//         console.log(result, resultSm)
-//         return result + '.' + resultSm
-//     } else if (num1s[1]) {
-//         let result = sumString(num1s[0], num2s[0])
-//         return result + '.' + num1s[1]
-//     } else if (num2s[1]) {
-//         let result = sumString(num1s[0], num2s[0])
-//         console.log(num1s[0], num2s[0])
-//         return result + '.' + num2s[1]
-//     } else {
-//         return sumString(num1s[0], num2s[0])
-//     }
-// }
-// // function test(num1, num2) {
-// //     const result = sumString(num1, num2)
-// //     return result
-// // }
-// console.log(add('0.1', '0.10'))
-
-function sum(n, total, callback) {
-    if (n === 0) {
-        callback(total)
-        return
+// 数组展平
+const myFlat = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            const left = arr.splice(0, i)
+            const right = arr.splice(1)
+            arr = left.concat(arr[0], right)
+            i--
+        }
     }
-    if (n % 1000 === 0) {
-        setTimeout(() => {
-            sum(n - 1, total + n, callback)
-        }, 0)
-    } else {
-        sum(n - 1, total + n, callback)
-    }
-
+    return arr
 }
 
-sum(100000000, 0, value => console.log(value))
+console.log(myFlat([1, 2, 3, [4, 5], 6, 7, [
+    [8, 9], 10
+]]))
+
+// 手写reduce
+Array.prototype.myReduce = function(fn, first) {
+    for (let i = 0; i < this.length; i++) {
+        first = fn(first, arr[i])
+    }
+    return first
+}
+
+const arr = [1, 2, 3]
+console.log(arr.myReduce((prev, item) => prev + item, 0))
+
+// 数组去重
+const clearArr1 = (arr) => {
+    return [...new Set(arr)]
+}
+
+const clearArr2 = (arr) => {
+    const hash = {}
+    for (let i = 0; i < arr.length; i++) {
+        if (!hash[arr[i]]) hash[arr[i]] = true
+    }
+    return Object.keys(hash).map(item => +item)
+}
+
+console.log(clearArr1([1, 1, 2, 3, 3, 4, 5, 6, 6]))
+console.log(clearArr2([1, 1, 2, 3, 3, 4, 5, 6, 6]))
+
+// 高阶函数
+const sum1 = (...args1) => {
+    if (args1.length > 2) {
+        return args1.reduce((prev, item) => prev + item, 0)
+    } else {
+        return function(...args2) {
+            return sum1(...args1, ...args2)
+        }
+    }
+}
+
+const sum2 = (...args1) => {
+    let nums = [...args1]
+    const result = (...args2) => {
+        if (args2.length === 0) return nums.reduce((prev, item) => prev + item, 0)
+        nums = nums.concat([...args2])
+        return result
+    }
+    return result
+}
+
+const sum3 = (...args1) => {
+    let nums = [...args1]
+    const result = function(...args2) {
+        nums = nums.concat([...args2])
+        return result
+    }
+    result.valueOf = function() {
+        return nums.reduce((prev, item) => prev + item, 0)
+    }
+    return result
+}
+
+const sum4 = (...args1) => {
+    let nums = [...args1]
+    const result = (...args2) => {
+        nums = nums.concat([...args2])
+        return result
+    }
+    result.toString = () => {
+        return nums.reduce((prev, item) => prev + item, 0)
+    }
+    return result
+}
+
+console.log(sum1(1, 2)(3))
+console.log(sum2(1, 2)(3)())
+console.log(sum3(1, 2)(3).valueOf())
+console.log(sum4(1, 2)(3))
+
+// 字符串全排列
+const allSort = (str) => {
+    const strArr = str.split('')
+    const result = []
+
+    function sort(prev, arr) {
+        if (arr.length === 1) {
+            result.push(prev + arr[0])
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                const nextArr = [...arr]
+                nextArr.splice(i, 1)
+                sort(prev + arr[i], nextArr)
+            }
+        }
+    }
+    sort('', strArr)
+    return result
+}
+
+console.log(allSort('abcd'))
+
+// 合并区间
+const merge = (arr) => {
+    arr = arr.sort((a, b) => a[0] - b[0])
+
+    function test(arr) {
+        arrCopy = [...arr]
+        for (let i = 1; i < arrCopy.length; i++) {
+            if (arrCopy[i - 1][1] >= arrCopy[i][0]) {
+                if (arrCopy[i - 1][1] < arrCopy[i][1]) {
+                    arrCopy[i - 1][1] = arrCopy[i][1]
+                }
+                arrCopy.splice(i, 1)
+                i--
+            }
+        }
+        if (arr.length === arrCopy.length) {
+            return arrCopy
+        }
+        return test(arrCopy)
+    }
+    return test(arr)
+}
+
+console.log(merge([
+    [1, 3],
+    [2, 6],
+    [15, 18],
+    [8, 10],
+    [10, 11],
+    [7, 8]
+]))
+
+// 观察者模式
+const Observer = (function() {
+    const _message = {}
+    return {
+        on: (type, fn) => {
+            if (_message[type]) {
+                _message[type].push(fn)
+            } else {
+                _message[type] = [fn]
+            }
+        },
+        subscribe: (type, args) => {
+            if (!_message[type]) return
+            _message[type].forEach(fn => {
+                fn(...args)
+            })
+        },
+        off: (type, fn) => {
+            if (!_message[type]) return
+            _message[type] = _message[type].filter(item => item !== fn)
+        }
+    }
+})()
