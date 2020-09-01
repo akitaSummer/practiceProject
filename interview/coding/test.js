3ec6218a5bc2629215c393eef1be13a9505043e2
akitasummer <644171127@qq.com>
2020-09-01 21:58:54 +0800

    20200831 21:58 update

--- interview/coding/test.js
+++ interview/coding/test.js
@@ -1,31 +1,43 @@
-class Task {
-    constructor() {
-        this.list = []
-        setTimeout(async() => {
-            for (let i = 0; i < this.list.length; i++) {
-                await new Promise((resolve, reject) => {
-                    const item = this.list[i]
-                    if (item.type === 'sleep') {
-                        setTimeout(() => {
-                            resolve()
-                        }, item.value)
-                    } else {
-                        console.log(item.value)
-                        resolve()
-                    }
-                })
+const turn2Coder = (arr) => {
+    let coderList = []
+    let count = 0
+    const com = (i, j) => {
+        if (arr[i - 1] && arr[i - 1][j]) arr[i - 1][j] = arr[i - 1][j] === 0 ? 0 : 2
+        if (arr[i + 1] && arr[i + 1][j]) arr[i + 1][j] = arr[i + 1][j] === 0 ? 0 : 2
+        if (arr[i][j - 1]) arr[i][j - 1] = arr[i][j - 1] === 0 ? 0 : 2
+        if (arr[i][j + 1]) arr[i][j + 1] = arr[i][j + 1] === 0 ? 0 : 2
+    }
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] === 1) {
+                let left = true
+                let top = true
+                let right = true
+                let bottom = true
+                if (arr[i - 1] && arr[i - 1][j]) left = arr[i - 1][j] === 0
+                if (arr[i + 1] && arr[i + 1][j]) left = arr[i + 1][j] === 0
+                if (arr[i][j - 1]) top = arr[i][j - 1] === 0
+                if (arr[i][j + 1]) bottom = arr[i][j + 1] === 0
+                if (top && right && left && bottom) return -1
             }
-        }, 0)
-        this.list = []
+        }
     }
-    sleep(time) {
-        this.list.push({ type: 'sleep', value: time })
-        return this
+    while (arr.map(item => item.some(people => people === 1)).some(item => item)) {
+        count++
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] === 2) {
+                    coderList.push({ i, j })
+                }
+            }
+        }
+        coderList.forEach(obj => com(obj.i, obj.j))
+        coderList = []
     }
-    print(string) {
-        this.list.push({ type: 'print', value: string })
-        return this
-    }
+    return count
 }
-
-(new Task()).print('hello').sleep(1000).print('world')
+console.log(turn2Coder([
+    [2, 1, 1],
+    [1, 1, 0],
+    [0, 1, 1]
+]))
