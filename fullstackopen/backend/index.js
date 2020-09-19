b748dfdbcabd6e5be83472adc7eb2458bf27f516
akitasummer <644171127@qq.com>
2020-09-19 23:36:53 +0800

    Exercises 3.7.-3.8.

--- fullstackopen/backend/index.js
+++ fullstackopen/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.json())
 
 let persons = [
