5108bc14d09bf7123dd55074ef72d039ec42bfb4
akitasummer <644171127@qq.com>
2021-10-25 23:30:31 +0800

    feat: add rollup

--- vite/rollup-test/index.js
+++ vite/rollup-test/index.js
@@ -1,11 +1,20 @@
 // rollup -i index.js rollup -i index.js --file dist.js
 // rollup -i index.js -i a.js --dir dist --format cjs --watch
 import path from 'path'
-import { funcA } from './a'
+import { funcA } from 'a'
+import React from 'react'
+import { add } from './b'
+
+import url from './logo.png'
 
 import pkg from './package.json'
 
 console.log(pkg)
+console.log(React)
+console.log(add(1, 2))
+console.log(__TEST__)
+
+console.log(url)
 
 funcA()
 console.log('Hello Rollup', path.join("", "/hello"))
--- vite/rollup-test/rollup.config.js
+++ vite/rollup-test/rollup.config.js
@@ -1,12 +1,62 @@
 // rollup --config rollup.config.js --environment TEST:123
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import alias from '@rollup/plugin-alias'
+import replace from '@rollup/plugin-replace'
+import ts from '@rollup/plugin-typescript'
+import image from '@rollup/plugin-image'
+import strip from '@rollup/plugin-strip'
 
 console.log(process.env.TEST)
 
-export default {
+// export default {
+//     input: 'index.js',
+//     output: {
+//         file: 'dist.js',
+//         format: 'umd',
+//         name: 'Index'
+//     }
+// }
+
+export default [{
     input: 'index.js',
-    output: {
-        file: 'dist.js',
+    external: ['react'],
+    // external: {
+    //     'react': 'React'
+    // },
+    plugins: [resolve(), alias({
+        entries: {
+            a: "./a"
+        }
+    }), commonjs(), replace({
+        __TEST__: 1234
+    }), image(), strip(), ts(), json()],
+    output: [{
+        file: 'dist/index.umd.js',
         format: 'umd',
-        name: 'Index'
+        name: 'Index',
+        plugins: [terser()],
+        banner: "/** Hello This is Banner **/"
+    }, {
+        file: 'dist/index.cjs.js',
+        format: 'cjs',
+        name: 'Index',
+        plugins: [terser()],
+        banner: "/** Hello This is Banner **/"
+    }]
+}, {
+    input: 'index.js',
+    external: ['react'],
+    plugins: [resolve(), commonjs(), replace({
+        __TEST__: 1234
+    }), image(), strip(), ts(), json()],
+    output: {
+        file: 'dist/index.es.js',
+        format: 'es',
+        name: 'Index',
+        plugins: [terser()],
+        banner: "/** Hello This is Banner **/"
     }
-}
+}]
